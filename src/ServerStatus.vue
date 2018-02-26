3b0f705ea70f5da79400b07c1da8475692e05c49
Matt Stover <mcs@mcstover.com>
2018-02-26 14:57:19 -0800

    Simple component concepts

---
+++ src/ServerStatus.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="server">
+  <h2>
+    Server {{ this.index }} - {{ status }}
+  </h2>
+  <br>
+  <button @click="changeStatus">Change</button>
+</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical'
+    }
+  },
+  props: [
+    'index'
+  ],
+  methods: {
+    changeStatus() {
+      this.status = 'Normal'
+    }
+  }
+}
+</script>
+
+<style>
+.server {
+  display: block;
+  margin: .5rem;
+  padding: .5rem;
+  border-radius: .25rem;
+  border: 1px solid #EEE;
+}
+</style>
