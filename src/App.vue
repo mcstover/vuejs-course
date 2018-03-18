b53b36bf1482441ec4ced32cffe67e6abaa966e1
Matt Stover <mcs@mcstover.com>
2018-03-18 14:46:56 -0700

    Sec 10 Quote App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
 	<div id="app">
-		<sec9-page></sec9-page>
+		<sec10-quotes></sec10-quotes>
+		<!-- <sec9-page></sec9-page>
 		<hr>
 		<sec8-page></sec8-page>
 		<hr>
-		<ex6-page></ex6-page>
+		<ex6-page></ex6-page> -->
 		<!-- Globally included Component -->
     	<!-- <simple-status></simple-status> -->
 	</div>
 </template>
 
 <script>
-import Ex6Page from './Sec7/Ex6Page.vue';
-import Sec8Page from './Sec8/Sec8Page.vue';
-import Sec9Page from './Sec9/Sec9Page.vue';
+// import Ex6Page from './Sec7/Ex6Page.vue';
+// import Sec8Page from './Sec8/Sec8Page.vue';
+// import Sec9Page from './Sec9/Sec9Page.vue';
+import Sec10Quotes from './Sec10/Sec10Page.vue';
 
 export default {
   name: 'app',
   components: {
-	  'ex6-page': Ex6Page,
-	  'sec8-page': Sec8Page,
-	  'sec9-page': Sec9Page
+	  'sec10-quotes': Sec10Quotes
+	//   'ex6-page': Ex6Page,
+	//   'sec8-page': Sec8Page,
+	//   'sec9-page': Sec9Page
   },
   data () {
     return {
---
+++ src/Sec10Quotes/Sec10Page.vue
@@ -0,0 +1,58 @@
+<template>
+  	<div class="sec10 grid-container">
+		<h2>Quotes Added</h2>
+		<div class="quote-meter"></div>
+
+		<!-- <sec10-new-quote></sec10-new-quote> -->
+
+		<!-- <sec10-quote-list></sec10-quote-list> -->
+
+		<div class="quote-info">
+			<p>INFO: Click on a quote to DELETE it.</p>
+		<!-- </div>
+		<button @click="selectedComponent = 'Sec9SlotQuote'">Slot Quote</button>
+		<button @click="selectedComponent = 'Sec9Author'">Author</button>
+		<button @click="selectedComponent = 'Sec9NewQuote'">New</button>
+		<hr>
+		{{selectedComponent}}
+		<hr>
+		<keep-alive>
+			<component :is="selectedComponent">
+				<h2 slot="title">{{ quoteTitle }}</h2>
+				<blockquote>Some interesting statement or quote.</blockquote>
+			</component>
+		</keep-alive> -->
+		<!-- <sec9-slot-quote>
+			<h2 slot="title">{{ quoteTitle }}</h2>
+			<blockquote>Some interesting statement or quote.</blockquote>
+		</sec9-slot-quote> -->
+    </div>
+</template>
+
+<script>
+// import Sec9SlotQuote from './Sec9SlotQuote.vue';
+// import Sec9Author from './Sec9Author.vue';
+import Sec10NewQuote from './Sec10NewQuote.vue';
+
+export default {
+	data: function() {
+		return {
+			quoteTitle: 'Dynamic Quote Title',
+			selectedComponent: 'Sec9SlotQuote'
+		}
+	},
+	components: {
+		// Sec9SlotQuote,
+		// Sec9Author,
+		Sec10NewQuote
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style>
+
+</style>
+
